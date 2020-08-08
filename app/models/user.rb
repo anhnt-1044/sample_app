478a49a2960bab09c35293d215175b2f93bcf978
anhnt-1044 <nguyen.trung.anh@sun-asterisk.com>
2020-08-08 13:51:37 +0700

    add model user

---
+++ app/models/user.rb
@@ -0,0 +1,22 @@
+class User < ApplicationRecord
+  VALID_EMAIL_REGEX = Settings.validations.user.email.REGEX
+
+  validates :email, presence: true,
+    length: {maximum: Settings.validations.user.email.max_length},
+    format: {with: VALID_EMAIL_REGEX},
+    uniqueness: true
+  validates :password, presence: true,
+    length: {minimum: Settings.validations.user.password.min_length}
+  validates :name, presence: true,
+    length: {maximum: Settings.validations.user.name.max_length}
+    
+  has_secure_password
+
+  before_save :downcase_email
+
+  private
+
+  def downcase_email
+    email.downcase!
+  end
+end
