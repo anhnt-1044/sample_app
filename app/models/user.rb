58c248c307e4383ac9d2052b9789e07599a1ab36
anhnt-1044 <nguyen.trung.anh@sun-asterisk.com>
2020-08-10 17:28:01 +0700

    done chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,16 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = Settings.validations.user.email.REGEX
+  PERMITTED_ATTR = %i(email password name password_confirmation).freeze
 
   validates :email, presence: true,
-    length: {maximum: Settings.validations.user.email.max_length},
-    format: {with: VALID_EMAIL_REGEX},
-    uniqueness: true
+                  length: {maximum: Settings.validations.user.email.max_length},
+                    format: {with: VALID_EMAIL_REGEX},
+                    uniqueness: true
   validates :password, presence: true,
-    length: {minimum: Settings.validations.user.password.min_length}
+              length: {minimum: Settings.validations.user.password.min_length}
   validates :name, presence: true,
-    length: {maximum: Settings.validations.user.name.max_length}
-    
+                   length: {maximum: Settings.validations.user.name.max_length}
+
   has_secure_password
 
   before_save :downcase_email
