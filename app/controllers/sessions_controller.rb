893c3c128cf0a7f34f9d7b54053ffd58b9983b3c
anhnt-1044 <nguyen.trung.anh@sun-asterisk.com>
2020-08-12 12:29:39 +0700

    add chapter 9: advanced login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,10 +2,11 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    user = User.find_by email: params[:session][:email].downcase
-    if user&.authenticate(params[:session][:password])
-      login user
-      redirect_to user
+    @user = User.find_by email: params[:session][:email].downcase
+    if @user&.authenticate(params[:session][:password])
+      login @user
+      remember_process @user, params[:session]
+      redirect_to @user
     else
       flash.now[:danger] = t ".new.danger"
       render :new
@@ -13,7 +14,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 end
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,7 +12,36 @@ module SessionsHelper
   end
 
   def log_out
+    forget current_user
     session.delete :user_id
-    current_user = nil
+  end
+
+  def remember user
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def remember_process user, session
+    session[:remember_me] == "1" ? remember(user) : forget(user)
+  end
+
+  def current_user
+    if user_id = session[:user_id]
+      current_user ||= User.find_by id: user_id
+    elsif user_id = cookies.signed[:user_id]
+      user = User.find_by id: user_id
+      if user&.authenticated? cookies[:remember_token]
+        login user
+        current_user = user
+      end
+    end
+    current_user
+  end
+
+  def forget user
+    user.forget
+    cookies.delete :user_id
+    cookies.delete :remember_token
   end
 end
